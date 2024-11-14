8d072114263c53b580fe99ce42dd951ff3b0bf2c
Saira Munawar <smunawar6@myseneca.ca>
2024-11-14 12:02:49 -0500

    Edited Assignment2.py - Added skeleton template

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,17 @@
-#Assignment 2
+#!/usr/bin/env python3
+#Project Name: Assignment 2
+#Group 3
+#Student Names: Saira Munawar
+
+import sys, os
+
+def management(filename):
+   '''This function organizes the file based on the user's input'''
+   '''Options are: alphabetical, dietary, transportation, program name, electives'''
+
+def generate_report(filename):
+   '''Takes the management report and generates it into a file and moves it to the SchoolFolder'''
+
+def validiation:
+   '''Validates users options, if the user picks incorrect options then print an error message'''
+
