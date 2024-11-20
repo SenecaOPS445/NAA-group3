b8d892d3cc9d20db0d91eaf2a457a0c8e6dd7d43
Saira Munawar <smunawar6@myseneca.ca>
2024-11-19 22:35:15 -0500

    Added some functions

--- assignment2.py
+++ assignment2.py
@@ -5,13 +5,20 @@
 
 import sys, os
 
+def school_folder(folder_path):
+    '''Creates the school folder if it does not exit.'''
+
 def management(filename):
    '''This function organizes the file based on the user's input'''
-   '''Options are: alphabetical, dietary, transportation, program name, electives'''
+   '''Options are: alphabetical, dietary, transportation, program name, elective'''
 
 def generate_report(filename):
-   '''Takes the management report and generates it into a file and moves it to the SchoolFolder'''
+   '''Takes the management report and generates it into a file and moves it to the school folder'''
 
 def validiation:
-   '''Validates users options, if the user picks incorrect options then print an error message'''
+   '''Validates users options, if the user picks incorrect options then print a>
+'''
+
+if __name__ == '__main__':
+    
 
