34c43172e5fa3abb2871ab6ab16e899c273563fb
Saira Munawar <smunawar6@myseneca.ca>
2024-11-21 10:57:13 -0500

    Edited assignment 2 - added usage and read_file()

--- assignment2.py
+++ assignment2.py
@@ -13,9 +13,27 @@ def school_folder(folder_path):
         os.mkdir(folder_path) #creates the folder otherwise
         return folder_path + ' has been created.'
 
-#def management(filename):
-#   '''This function organizes the file based on the user's input'''
-#   '''Options are: alphabetical, dietary, transportation, program name, elective'''
+def usage():
+    '''prints messages to users'''
+    if len(sys.argv) != 2:
+        print('File name not given, Please enter a file name')
+    else:
+        file_name = sys.argv[1]
+        file_content = read_text_file(file_name)
+        print(file_content)
+
+def read_text_file(file_name):
+    '''opens the file given by the user'''
+    try:
+        management(file_name) #hmm?
+    except FileNotFoundError:
+        return 'File not found'
+
+def management(file_name):
+    #   '''This function organizes the file based on the user's input'''
+    #   '''Options are: alphabetical, dietary, transportation, program name, elective'''
+    f = open(file_name, 'r')
+    #categorizes
 
 #def generate_report(filename):
 #   '''Takes the management report and generates it into a file and moves it to the school folder'''
@@ -27,4 +45,5 @@ def school_folder(folder_path):
 if __name__ == '__main__':
     folder_path = 'school_folder' #the path of the folder - should be in the same directory as assignment 2
     print(school_folder(folder_path))
+    usage()
 
