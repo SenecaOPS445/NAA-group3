01edade99e035c68f5f1adf027910617ed05a638
ngharti-kore <nishangharti381@gmail.com>
2024-11-28 11:15:30 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -45,27 +45,4 @@ def find_student(file_name):
   
 
 
-import sys, os
-
-def campus(file_name, campuses):
-    try:
-        with open("user_test", "r") as file:
-            data = file.readlines()  # Read all lines from the file
-
-        # Initialize an empty dictionary to store campus counts
-        campus_details = {}
-
-        # Process each line in the file
-        for line in data:
-            campus_comp = [comp.strip(" '") for comp in line.strip().split(",")]
-            if len(campus) < 8:  # Ensure there are enough fields
-                continue  # Skip invalid lines with insufficient data
-
-            campus = campus_comp[2]
-
-            # Count occurrences of each transportation mode
-            campus_details[campus] = campus_details.get(campus, 0) + 1
-
-    except:
-            return campus_details  # Return the summary as a dictionary
 
